c05113a91fa0f8a8e81dbac3b8b1632268b66083
rijimuge <david.russell04@gmail.com>
2024-04-26 13:59:59 -0500

    add placeholder for blog section

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+    return <h1>This is a placeholder for a blog while I figure out how I want this to work.</h1>
+  }
--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -41,9 +41,21 @@ export function NavBar() {
               -&gt;
             </span>
           </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            some ai projects
-          </p>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">projects</p>
+        </Link>
+
+        <Link
+          href="/blog"
+          passHref
+          className="group rounded-lg border border-transparent px-2 py-2 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        >
+          <h2 className="mb-2 text-xl font-semibold">
+            blog
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">what I say</p>
         </Link>
       </div>
     </nav>
