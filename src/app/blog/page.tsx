01578eadf34ee3283079f7c122054fed87f1629d
rijimuge <david.russell04@gmail.com>
2024-04-28 15:14:06 -0500

    blog entry should I start a blog

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,16 @@
+import Link from "next/link";
+
 export default function Page() {
-    return <h1>This is a placeholder for a blog while I figure out how I want this to work.</h1>
-  }
+  return (
+    <div>
+      <h1 className ="py-2">In which thoughts, opinions and sometimes knowledge are shared.</h1>
+      <Link
+        href="blog/should-i-start-a-blog"
+        passHref
+        className="py-4 underline"
+      >
+        <h2 className="mb-2 text-xl font-semibold">Should I start a blog?</h2>
+      </Link>
+    </div>
+  );
+}
---
+++ src/app/blog/should-i-start-a-blog/page.tsx
@@ -0,0 +1,56 @@
+export default function Page() {
+  return (
+    <div className="blog-content">
+      <h2 className="mb-2 text-xl font-semibold">
+        Should I Start a Blog?
+      </h2>
+      <div className ="indent-4 opacity-60">April 28 2024</div>
+      <p>
+        Apparently, yes! Here it is, such as it is. Let me explain how I reached
+        my decision.
+      </p>
+      <p>
+        For the last 10 years, I&apos;ve had a writing practice that consisted
+        of writing anything that came into my mind for 30 minutes a day in the
+        morning. I started this after listening to a Tim Ferris podcast where
+        the guest recommended everyone write something, anything, first thing in
+        the mornings. I tried it out and liked it, and have mostly kept it up
+        since then.{" "}
+      </p>
+      <p>
+        Just the writing itself is valuable. It offers an opportunity to give
+        form to the thoughts bouncing around in my head, that potentially
+        influence me. Left alone, without the benefit of the critical reflection
+        that may be necessary to lend the thought a mooring in reality, morality
+        or rationality, these thoughts could be downright destructive! Or
+        potentially life-changing ideas may never receive the attention to
+        flourish from their incipient state into fully formed notions.
+      </p>{" "}
+      <p>
+        One of the best things about starting a blog is, whatever you&apos;re
+        hoping to achieve, it may still be a good idea. A blog, at its best, is
+        something like an essay, and essays have been around in some form for
+        thousands of years. Why? Because people find value in writing them, and
+        people find value in reading them.{" "}
+      </p>
+      <p>
+        Blogs are an amazingly resilient form if you define them as an evolution
+        of the essay. They have remained a viable business model unto
+        themselves, in the 21st century through all manner of disruptions,
+        technological and otherwise, proving that there is an inherent value to
+        the form that will survive into the foreseeable future.{" "}
+      </p>{" "}
+      <p>
+        However, I&apos;m not trying to persuade you to start a blog--if
+        anything, I&apos;m trying to persuade you to read mine. And with good
+        reason. The hope is that more attention, more eyes on my blog, will lead
+        to more opportunities.{" "}
+      </p>
+      <p>
+        But, honestly, I think what appeals to me most is the notion that my
+        thoughts, the stuff of my conscious existence, will be shared with and
+        maybe even processed by other people.
+      </p>
+    </div>
+  );
+}
